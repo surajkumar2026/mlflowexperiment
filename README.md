a4131b13a9a2e8914024a89a08246103246507c5
Surajkumar9798 <me22b1073@iiitdm.ac.in>
2025-03-26 11:44:52 +0530

    mlfow_tracking_with_dagshub

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## ML Fow experiments
+
+MLFLOW_TRACKING_URI=https://dagshub.com/surajkumar2026/mlflowexperiment.mlflow \
+MLFLOW_TRACKING_USERNAME=surajkumar2026 \
+MLFLOW_TRACKING_PASSWORD=45079e57743bb72a08a822acc414e38516374b88 \
+python script.py
--- app.py
+++ app.py
@@ -55,6 +55,9 @@ if __name__ == "__main__":
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
     l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
 
+    import dagshub
+    dagshub.init(repo_owner='surajkumar2026', repo_name='mlflowexperiment', mlflow=True)
+
     with mlflow.start_run():
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
         lr.fit(train_x, train_y)
@@ -73,6 +76,11 @@ if __name__ == "__main__":
         mlflow.log_metric("rmse", rmse)
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
+
+        ##for remote server only (DAgshub)
+       
+        remote_server_uri = "https://dagshub.com/surajkumar2026/mlflowexperiment.mlflow"
+        mlflow.set_tracking_uri(remote_server_uri)
 
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
